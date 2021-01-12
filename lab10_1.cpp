1a7f1f9cf414a8f61f9763d2b7019b145ff33778
Kisaru1523 <76036259+Kisaru1523@users.noreply.github.com>
2021-01-12 19:34:15 +0700

    lab10_1.cpp

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,19 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+	srand(time(0));
+	cout << "Press Enter 3 times to reveal your future.";
+	cin.get();
+	cin.get();
+	cin.get();
+	string x[]={"A","B+","B","C+","C","D+","D","F","W"};
+	int y=rand()%9;
+	cout << "You will get "<<x[y]<<" in this 261102.";
+	return 0;
+}
